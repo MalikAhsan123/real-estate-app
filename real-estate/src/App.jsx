23e1b5266f9cb9e309d2aec0b67b4f970e0a3761
qamaramin006 <130451218+MalikAhsan123@users.noreply.github.com>
2025-04-08 18:26:29 +0500

    g
    jj

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
+<<<<<<< Updated upstream
 import Navbar from './components/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+=======
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import about from './modules/about/About.jsx'
+// import './App.css'
+>>>>>>> Stashed changes
 
 function App() {
-  
+
 
   return (
     <>
+<<<<<<< Updated upstream
      <Navbar />
      <Outlet />
      
      
+=======
+      {/* <h1 className='bg-red-500'>AhSAN</h1> */}
+      {/* <about /> */}
+>>>>>>> Stashed changes
     </>
   )
 }
