a4e3b69880cd3528846c5ad0f748332d3961749e
qamaramin006 <ahsanraza2408@gmail.com>
2025-06-03 10:11:28 +0500

    js
    js

--- real-estate/src/AdminModules/adminDashboard/AdminDashboard.jsx
+++ real-estate/src/AdminModules/adminDashboard/AdminDashboard.jsx
@@ -1,18 +1,18 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import Dashboard from "../Dashboard/Dashboard";
-// import UserManagement from "../UserManagement/UserManagement";
-// // import UserManagement from "./pages/UserManagement";
-// // import PropertyManagement from "./pages/PropertyManagement";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import UserManagement from "../UserManagement/UserManagement";
+// import UserManagement from "./pages/UserManagement";
+// import PropertyManagement from "./pages/PropertyManagement";
 
-// const AdminDashboard = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Dashboard />} />
-//        <Route path="/users" element={<UserManagement />} />
-//       {/* <Route path="/properties" element={<PropertyManagement />} /> */}
-//     </Routes>
-//   );
-// };
+const AdminDashboard = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+       <Route path="/users" element={<UserManagement />} />
+      {/* <Route path="/properties" element={<PropertyManagement />} /> */}
+    </Routes>
+  );
+};
 
-// export default AdminDashboard;
+export default AdminDashboard;
